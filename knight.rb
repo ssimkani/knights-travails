5ceeb294c41a0c778c037d2871733b26f2081fef
ssimkani <seenasimkani@gmail.com>
2024-06-26 14:16:55 -0400

    Add knight_moves to knight class

--- knight.rb
+++ knight.rb
@@ -3,9 +3,20 @@
 require_relative 'graph'
 
 class Knight
-  attr_reader :graph
-
   def initialize
-    @graph = Graph.new
+    @graph = create_graph
+  end
+
+  def create_graph
+    graph = Graph.new
+    graph.create_nodes
+    graph.create_edges
+    graph
+  end
+
+  def knight_moves(start_node, end_node)
+    shortest_path = @graph.shortest_path(start_node, end_node)
+    puts "You made it in #{shortest_path.length - 1} moves! Heres your path:\t\t"
+    shortest_path.each { |coord| p coord }
   end
 end
